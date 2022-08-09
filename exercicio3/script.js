1388b668a9f36cacf822989f7d27f139acf9f72b
Joanna Souza <joannasouza40@gmail.com>
2022-08-09 17:58:23 -0300

    Exercícios feitos!

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,5 +1,5 @@
 //código a ser reescrito
-
+/*
 if(nome === "José"){
 	console.log("Oi, Zé!");
 } else {
@@ -10,4 +10,13 @@ if(idade >= 18){
 	console.log("pode tirar carteira de motorista!");
 } else {
 	console.log("Ainda não pode tirar carteira de motorista");
-}
+}*/
+
+const nome = prompt("Qual seu nome?")
+
+nome === "José" ? console.log("Oi, Zé!") : console.log("Olá,", nome);
+
+const idade = prompt("Qual sua idade?")
+
+idade >= 18 ? console.log("Pode tirar a carteira de motorista!") : console.log("Ainda não pode tirar a carteira de motorista!")
+
